370aaf3a1a7352a49e17c87e37e926be8a1ca938
Shay Dubrovsky <shaydu9@gmail.com>
2018-08-15 15:36:36 +0300

    End of lesson_03

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Shay Dubrovsky on 15/08/2018.
+//  Copyright © 2018 Shay Dubrovsky. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
